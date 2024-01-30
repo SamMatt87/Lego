f92a6cecaaef7b3453c0bb7575d21818f95ce6f4
SamMatt87 <sam.matthews.77@gmail.com>
2024-01-30 11:31:04 +1100

    Uncomment code

--- data_exploration.py
+++ data_exploration.py
@@ -24,33 +24,33 @@ for count in range(0,41):
 for theme in themes:
     print(len(rebrickable_api.show_sets(theme)))
     for set in rebrickable_api.show_sets(theme):
-        # time.sleep(1)
-        # year_count[set['year']] +=1
+        time.sleep(1)
+        year_count[set['year']] +=1
         if set['num_parts']<1000:
             part_count[int(np.floor(set['num_parts']/100))*100] += 1
         else:
             part_count[int(np.floor(set['num_parts']/1000))*1000] += 1
-        # if set['num_parts']>0:
-        #     unique_parts[np.round((rebrickable_api.unique_parts(set['set_num'])/set['num_parts']),1)]+=1
-        # else:
-        #     print(set)
-        # time.sleep(1)
-        # mocs = rebrickable_api.moc_count(set['set_num'])
-        # moc_counts[mocs] += 1
-# print(year_count)
+        if set['num_parts']>0:
+            unique_parts[np.round((rebrickable_api.unique_parts(set['set_num'])/set['num_parts']),1)]+=1
+        else:
+            print(set)
+        time.sleep(1)
+        mocs = rebrickable_api.moc_count(set['set_num'])
+        moc_counts[mocs] += 1
+print(year_count)
 print(part_count)
-# print(unique_parts)
-# print(moc_counts)
-# print(sum(year_count.values()))
-# print(sum(part_count.values()))
-# print(sum(unique_parts.values()))
-# print(sum(moc_counts.values()))
+print(unique_parts)
+print(moc_counts)
+print(sum(year_count.values()))
+print(sum(part_count.values()))
+print(sum(unique_parts.values()))
+print(sum(moc_counts.values()))
 def plot(data: Dict[int,int]) -> None:
     plt.bar(range(len(data)), list(data.values()), align="center")
     plt.xticks(range(len(data)), list(data.keys()))
     plt.show()
 
-# plot(year_count)
+plot(year_count)
 plot(part_count)
-# plot(unique_parts)
-# plot(moc_counts)
+plot(unique_parts)
+plot(moc_counts)
